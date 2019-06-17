69c2e870fad411fe1e6009569b215a8d7959fb2d
axecopfire <sbraun990@gmail.com>
2019-06-17 15:24:32 -0400

    first day of Javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict"
+
+
+
+var invJams = 1;
+var invJellies = 2;
+var invGrapes = 3;
+var guavas = false;
+
+var groceryCart = "1 jellies, 1 jams, 1 grapes";
+
+invJams = 0;
+invJellies = 1;
+invGrapes = 2;
+
+console.log("This is guavas", guavas);
+
+console.log("This is in our grocery cart", groceryCart);
+console.log("Our Inventory is now", invJams, invJellies, invGrapes);
+
+// // groceryCart = 'jams, grapes';
+// var groceryCartWithoutJellies = 'jams, grapes';
+
+// console.log(groceryCartWithoutJellies, groceryCart);
+
+// var groceryCartWithBreadAndPeanutButter = "jams, jellies, grapes, bread, peanutButter";
+// // console.log(groceryCart);
+
+// console.log("123000000000000000000000000000000000000000000000000000000000000000000000000000000000012");
